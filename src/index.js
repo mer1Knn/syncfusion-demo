3c061e0e7935c4f23edf692ac59931c944c4e85f
Merouane Kanoune <kanounemerouane@gmail.com>
2023-03-21 09:11:48 +0100

    syncfusion-demo

--- src/index.js
+++ src/index.js
@@ -1,17 +1,22 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
+import * as ReactDOM from 'react-dom';
+import * as React from 'react';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+import { PdfViewerComponent, Toolbar, Magnification, Navigation, LinkAnnotation, BookmarkView,
+         ThumbnailView, Print, TextSelection, Annotation, TextSearch, FormFields, FormDesigner, Inject} from '@syncfusion/ej2-react-pdfviewer';
+export function App() {
+return (<div>
+    <div className='control-section'>
+        <PdfViewerComponent 
+            id="container" 
+            documentPath="PDF_Succinctly.pdf" 
+            serviceUrl="https://ej2services.syncfusion.com/production/web-services/api/pdfviewer" 
+            style={{ 'height': '1000px' }}>
+               {/* Inject the required services */}
+               <Inject services={[ Toolbar, Magnification, Navigation, Annotation, LinkAnnotation, BookmarkView, ThumbnailView,
+                                   Print, TextSelection, TextSearch, FormFields, FormDesigner]} />
+        </PdfViewerComponent>
+    </div>
+</div>);
+}
+const root = ReactDOM.createRoot(document.getElementById('sample'));
+root.render(<App />);
